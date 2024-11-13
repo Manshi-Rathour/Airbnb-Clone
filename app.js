696a1546d2d5d7da94eba13863ae78146b4c207e
name <manshi2086@gmail.com>
2024-11-13 19:35:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// step 1
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+});
+
+// step 4 (index route)
+
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+
+// step 2 
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new villa",
+//         description: "by the beach",
+//         image: "https://plus.unsplash.com/premium_photo-1686090450800-d6ca456243c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+
+// });
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
+
---
+++ models/listing.js
@@ -0,0 +1,41 @@
+// step 2
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,  
+        trim: true      
+    },
+    description: {
+        type: String,
+        trim: true     
+    },
+    image: {
+        type: String,
+        default: "https://plus.unsplash.com/premium_photo-1686090450800-d6ca456243c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1686090450800-d6ca456243c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+    },
+    price: {
+        type: Number,
+        min: [0, "Price must be a positive number"], 
+        required: true
+    },
+    location: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    country: {
+        type: String,
+        trim: true,
+        required: true
+    }
+});
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
