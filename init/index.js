9b827d301806b52829166f2ad27395eb49de7588
name <manshi2086@gmail.com>
2024-12-05 20:52:22 +0530

    phase 2 completed (user authentication and authorization done)

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: '67502ce9137c78376935b42a'}));    
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
