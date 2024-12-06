27fda8ed12e7f6a598f8bd579899bd2f41cc6af5
name <manshi2086@gmail.com>
2024-12-06 21:02:53 +0530

    image upload feature added in show.ejs and img will be stored in cloud storage cloudinary

--- models/listing.js
+++ models/listing.js
@@ -13,9 +13,8 @@ const listingSchema = new Schema({
         trim: true
     },
     image: {
-        type: String,
-        default: "https://plus.unsplash.com/premium_photo-1686090450800-d6ca456243c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1686090450800-d6ca456243c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+        url: String,
+        filename: String
     },
     price: {
         type: Number,
--- routes/listing.js
+++ routes/listing.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
-
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 // index route
 router.get("/", wrapAsync(listingController.index));
@@ -17,7 +18,7 @@ router.get("/new", isLoggedIn, listingController.renderNewForm);
 router.get("/:id", wrapAsync(listingController.showListing));
 
 // create route
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+router.post("/", isLoggedIn, upload.single('listing[image]'), validateListing, wrapAsync(listingController.createListing));
 
 // edit route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
