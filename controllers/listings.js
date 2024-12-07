dc0c903d6b4a1fb1f116007d537e407f5fbc6e41
name <manshi2086@gmail.com>
2024-12-07 11:56:25 +0530

    edit page updated to upload new image and show original image

--- controllers/listings.js
+++ controllers/listings.js
@@ -46,13 +46,26 @@ module.exports.renderEditForm = async (req, res) => {
         req.flash("error", "Listing you requested for does't exist!");
         res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
+
+    let originalImgUrl = listing.image.url;
+    originalImgUrl = originalImgUrl.replace("/upload", "/upload/w_300");
+
+    res.render("listings/edit.ejs", { listing, originalImgUrl });
 };
 
 
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    if(typeof req.file !== "undefined"){
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = {url, filename};
+        await listing.save();
+    }
+
+
     req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 };
