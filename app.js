52b8927b13986ef282359c2f7b809adf0f57fe29
name <manshi2086@gmail.com>
2024-12-16 17:45:34 +0530

    updated app.js

--- app.js
+++ app.js
@@ -70,7 +70,8 @@ const sessionOptions = {
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true
+        // httpOnly: true
+        secure:false,
     }
 };
 
